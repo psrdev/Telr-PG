a2add6773d95803c57ba0bd1ab758158a1bc1a7a
Pravin Singh Rana <dev.psrana@gmail.com>
2025-03-28 16:56:23 +0530

    fixed the spaceing

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -6,24 +6,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook/telr', (req, res) => {
     console.log('Webhook received from server');
-
-
-
-
-
-
-
-
     const hashCheck = signData(req.body);
     if (hashCheck) {
-        // we can send mail to the client here and 
+        // we can send mail to the client here 
         console.log("Hash check passed");
     }
-
-
-
-
-
     res.status(200).json({ status: 'success' });
 });
 
